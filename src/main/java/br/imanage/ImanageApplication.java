e0dffad54c2ba6e3cd671bb07f8cff99f8caeea9
Conrado <conradocjo@gmail.com>
2024-04-01 07:42:44 -0300

    Commit inicial

---
+++ src/main/java/br/imanage/ImanageApplication.java
@@ -0,0 +1,13 @@
+package br.imanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImanageApplication.class, args);
+	}
+
+}
