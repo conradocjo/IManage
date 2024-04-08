a4145cb700d8ec8be2290d89fa45bafc306a13f7
Conrado <conradocjo@gmail.com>
2024-04-07 21:09:17 -0300

    Ajuste properties

---
+++ src/main/java/br/imanage/config/PropertiesConfig.java
@@ -0,0 +1,18 @@
+package br.imanage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class PropertiesConfig {
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer configurer() {
+        var configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource("./application.properties"));
+        return configurer;
+    }
+
+}
